ee6bd659001ec445f233d3691d81fee9562400d8
Yusriyah IM <yusriyahisna24@gmail.com>
2021-06-18 23:58:37 +0700

    add function & container

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
     addTodo();
   });
 });
+
+function makeTodo() {
+  const textTitle = document.createElement("h2");
+  textTitle.innerText = "Tugas Android";
+
+  const textTimestamp = document.createElement("p");
+  textTimestamp.innerText = "2021-05-01";
+
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("inner");
+  textContainer.append(textTitle, textTimestamp);
+
+  const container = document.createElement("div");
+  container.classList.add("item", "shadow");
+  container.append(textContainer);
+
+  return container;
+}
